9ecc3da2e2811bb6f5948daaec38447d4007878e
Seth Rogers <sethhrogers@gmail.com>
2017-09-12 16:32:57 -0700

    Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikiService } from './wiki.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items: Array<string>;
+
+  constructor(private wikiService: WikiService) {}
+
+  search(term) {
+    this.wikiService.search(term)
+      .then(items => this.items = items);
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WikiService } from './wiki.service';
+import { JsonpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JsonpModule
   ],
-  providers: [],
+  providers: [WikiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
